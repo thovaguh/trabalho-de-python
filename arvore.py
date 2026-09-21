794e45bf4b5fc76574dd240502add462708e96f1
thovaguh <gusthavoerick09@gmail.com>
2026-09-21 16:42:06 -0300

    correções

--- arvore.py
+++ arvore.py
@@ -1,44 +1,48 @@
-class No: #esse é o bloco de memoria que vai armazenar a chave e o offset do arquivo
+# nó
+class No:
     def __init__(self, chave, offset):
         self.chave = int(chave)
         self.offset = int(offset)
         self.esquerda = None
         self.direita = None
 
-# o self é como: guarde.essa variavel = nessa variavel
-class ArvoreBinaria: # essa parte é a arvore binaria que vai armazenar os blocos de memoria
+# arvore
+class ArvoreBinaria:
     def __init__(self):
         self.raiz = None
 
-    def inserir(self, chave, offset): #aqui é a função que vai inserir uma nova chave e seu offset no arquivo
+    # inserir
+    def inserir(self, chave, offset):
         if self.raiz is None:
             self.raiz = No(chave, offset)
         else:
-            self._inserir_recursivo(self.raiz, chave, offset)#aqui temos um else que chama a função recursiva para inserir a chave e o offset no lugar correto da arvore
+            self._inserir_recursivo(self.raiz, chave, offset)
 
-    def _inserir_recursivo(self, no_atual, chave, offset):#aqui é a função recursiva que vai percorrer a arvore e inserir a chave e o offset no lugar correto
+    def _inserir_recursivo(self, no_atual, chave, offset):
+        chave = int(chave)
         if chave < no_atual.chave:
             if no_atual.esquerda is None:
                 no_atual.esquerda = No(chave, offset)
             else:
-                self._inserir_recursivo(no_atual.esquerda, chave, offset)#esse else chama a função recursiva novamente para percorrer a arvore e encontrar o lugar correto para inserir a chave e o offset
+                self._inserir_recursivo(no_atual.esquerda, chave, offset)
         elif chave > no_atual.chave:
             if no_atual.direita is None:
-                no_atual.direita = No(chave, offset)#esse elif chama a função recursiva novamente para percorrer a arvore e encontrar o lugar correto para inserir a chave e o offset
+                no_atual.direita = No(chave, offset)
             else:
-                self._inserir_recursivo(no_atual.direita, chave, offset)#aqui testa se a chave é maior que a chave do nó atual, se for, ele vai para a direita da arvore e chama a função recursiva novamente para percorrer a arvore e encontrar o lugar correto para inserir a chave e o offset
+                self._inserir_recursivo(no_atual.direita, chave, offset)
         else:
             print(f"Erro: Chave {chave} já existe no índice.")
 
-    def buscar(self, chave):#esse trecho é a função que vai buscar uma chave no arquivo e retornar o offset correspondente
-        return self._buscar_recursivo(self.raiz, chave)
+    # buscar
+    def buscar(self, chave):
+        return self._buscar_recursivo(self.raiz, int(chave))  # Converte para int antes de iniciar a busca recursiva
 
     def _buscar_recursivo(self, no_atual, chave):
         if no_atual is None:
-            return None  # Chave não encontrada
+            return None 
         
         if chave == no_atual.chave:
-            return no_atual.offset  # Encontrou! Retorna a posição no arquivo.
+            return no_atual.offset
         elif chave < no_atual.chave:
             return self._buscar_recursivo(no_atual.esquerda, chave)
         else:
--- gerenciador.py
+++ gerenciador.py
@@ -1,90 +1,73 @@
 import os
 from modelos import Idioma, Licao, Exercicio, Usuario
 
-#     IDIOMAS.TXT
+# ==========================================
+#               IDIOMAS.TXT
+# ==========================================
+
 def salvar_idioma(idioma, arq_nome="idiomas.txt"):
-    # O bloco 'with' abre o arquivo e garante o fechamento automático no final (dispensa o fclose).
-    # O modo "a+" (Append) abre para adição, protegendo os dados antigos e indo para o final.
     with open(arq_nome, "a+", encoding="utf-8") as arq:
-        arq.seek(0, os.SEEK_END) # Força a agulha ir para o último byte do arquivo
-        
-        # tell() é o "GPS": pega a coordenada exata em bytes de onde a linha vai começar
-        offset = arq.tell() 
+        arq.seek(0, os.SEEK_END)  
+        offset = arq.tell()  
         
         linha = f"{idioma.codigo},{idioma.descricao}\n"
         arq.write(linha)
         
-        return offset # Devolvemos a coordenada para a Árvore Binária guardar na RAM
-
+        return offset
 
 def ler_idioma_offset(offset, arq_nome="idiomas.txt"):
-    # O modo "r" (Read) abre apenas para leitura.
     with open(arq_nome, "r", encoding="utf-8") as arq:
-        # seek() é o "Teletransporte": pula instantaneamente para a coordenada (offset) passada
         arq.seek(offset) 
-        
-        linha = arq.readline() # Lê apenas a linha daquela coordenada
+        linha = arq.readline() 
         
         if not linha:
             return None
         
-        # strip() limpa sujeiras como o "\n" do final da linha
-        # split(",") corta a string onde tem vírgula, transformando em um vetor: ["1", "Ingles"]
         codigo, descricao = linha.strip().split(",")
-        
         return Idioma(codigo, descricao)
 
-
 def carregar_indices_idiomas(arvore, arq_nome="idiomas.txt"):
-    # Evita erro na primeira vez que o programa roda e o arquivo ainda não existe
     if not os.path.exists(arq_nome):
         return
 
     with open(arq_nome, "r", encoding="utf-8") as arq:
         while True:
-            # Captura a posição em bytes (GPS) ANTES de ler a linha atual
             offset = arq.tell()
-            
             linha = arq.readline()
             
-            # Se a linha vier vazia, chegamos ao Fim do Arquivo (EOF), então quebra o laço
             if not linha:
                 break
             
-            # Separa os dados pelo delimitador (vírgula)
             partes = linha.strip().split(",")
-            
             if len(partes) >= 2:
-                codigo = int(partes[0]) # Pega apenas a chave primária (ID)
-                
-                # Reconstrói a estrutura na memória RAM inserindo a chave e a coordenada (offset)
+                codigo = int(partes[0])
                 arvore.inserir(codigo, offset)
 
 
-#   LIÇÕES.TXT
+# ==========================================
+#               LIÇÕES.TXT
+# ==========================================
 
 def salvar_licao(licao, arq_nome="licoes.txt"):
     with open(arq_nome, "a+", encoding="utf-8") as arq:
         arq.seek(0, os.SEEK_END)
         offset = arq.tell()
         
-        # Salva os 3 atributos: ID da lição, Título e ID do Idioma pai
         linha = f"{licao.codigo},{licao.codigo_idioma},{licao.niveis_total}\n"
         arq.write(linha)
         
         return offset
 
-
 def ler_licao_offset(offset, arq_nome="licoes.txt"):
     with open(arq_nome, "r", encoding="utf-8") as arq:
         arq.seek(offset)
         linha = arq.readline()
+        
         if not linha:
             return None
         
         codigo, codigo_idioma, niveis_total = linha.strip().split(",")
         return Licao(codigo, codigo_idioma, niveis_total)
-
 
 def carregar_indices_licoes(arvore, arq_nome="licoes.txt"):
     if not os.path.exists(arq_nome):
@@ -94,38 +77,40 @@ def carregar_indices_licoes(arvore, arq_nome="licoes.txt"):
         while True:
             offset = arq.tell()
             linha = arq.readline()
+            
             if not linha:
                 break
+                
             partes = linha.strip().split(",")
             if len(partes) >= 3:
                 codigo = int(partes[0])
                 arvore.inserir(codigo, offset)
 
 
-#   EXERCÍCIOS.TXT
+# ==========================================
+#             EXERCÍCIOS.TXT
+# ==========================================
 
 def salvar_exercicio(exercicio, arq_nome="exercicios.txt"):
     with open(arq_nome, "a+", encoding="utf-8") as arq:
         arq.seek(0, os.SEEK_END)
         offset = arq.tell()
         
-        # Salva os 4 atributos: ID do exercício, Enunciado, Resposta e ID da Lição pai
         linha = f"{exercicio.codigo},{exercicio.codigo_licao},{exercicio.nivel_dificuldade},{exercicio.descricao},{exercicio.opcoes_resposta},{exercicio.resposta_correta},{exercicio.pontuacao}\n"
         arq.write(linha)
         
         return offset
 
-
 def ler_exercicio_offset(offset, arq_nome="exercicios.txt"):
     with open(arq_nome, "r", encoding="utf-8") as arq:
         arq.seek(offset)
         linha = arq.readline()
+        
         if not linha:
             return None
         
         codigo, codigo_licao, nivel_dificuldade, descricao, opcoes_resposta, resposta_correta, pontuacao = linha.strip().split(",")
         return Exercicio(codigo, codigo_licao, nivel_dificuldade, descricao, opcoes_resposta, resposta_correta, pontuacao)
-
 
 def carregar_indices_exercicios(arvore, arq_nome="exercicios.txt"):
     if not os.path.exists(arq_nome):
@@ -135,43 +120,48 @@ def carregar_indices_exercicios(arvore, arq_nome="exercicios.txt"):
         while True:
             offset = arq.tell()
             linha = arq.readline()
+            
             if not linha:
                 break
+                
             partes = linha.strip().split(",")
             if len(partes) >= 7:
                 codigo = int(partes[0])
                 arvore.inserir(codigo, offset)
 
 
-#   USUÁRIOS.TXT
+# ==========================================
+#              USUÁRIOS.TXT
+# ==========================================
 
 def salvar_usuario(usuario, arq_nome="usuarios.txt"):
-   def salvar_usuario(usuario):
-    with open("usuarios.txt", "a+", encoding="utf-8") as arq:
-        arq.seek(0, 2)
+    with open(arq_nome, "a+", encoding="utf-8") as arq:
+        arq.seek(0, os.SEEK_END)
         offset = arq.tell()
-        # Adicionamos a 6ª coluna no final para salvar os concluidos
+        
         linha = f"{usuario.codigo},{usuario.nome},{usuario.codigo_idioma},{usuario.nivel_atual},{usuario.pontuacao_total},{usuario.exercicios_concluidos}\n"
         arq.write(linha)
-    return offset
+        
+        return offset
 
-def ler_usuario_offset(offset):
-    with open("usuarios.txt", "r", encoding="utf-8") as arq:
+def ler_usuario_offset(offset, arq_nome="usuarios.txt"):
+    with open(arq_nome, "r", encoding="utf-8") as arq:
         arq.seek(offset)
         linha = arq.readline().strip()
+        
+        if not linha:
+            return None
+            
         partes = linha.split(",")
         
-        # Lê todas as partes com segurança
         cod = int(partes[0])
         nome = partes[1]
         cod_idioma = int(partes[2])
         nivel = int(partes[3])
         pts = int(partes[4])
-        # Pega a lista de concluídos (se existir)
         concluidos = partes[5] if len(partes) > 5 else ""
         
         return Usuario(cod, nome, cod_idioma, nivel, pts, concluidos)
-
 
 def carregar_indices_usuarios(arvore, arq_nome="usuarios.txt"):
     if not os.path.exists(arq_nome):
@@ -181,8 +171,10 @@ def carregar_indices_usuarios(arvore, arq_nome="usuarios.txt"):
         while True:
             offset = arq.tell()
             linha = arq.readline()
+            
             if not linha:
                 break
+                
             partes = linha.strip().split(",")
             if len(partes) >= 5:
                 codigo = int(partes[0])
